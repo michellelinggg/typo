38fde07d6ee45b21dc4e2a86479f9bbbe3549b62
Michelle <linglinglikerice@gmail.com>
2015-03-07 14:53:02 -0800

    minor change

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -29,6 +29,7 @@ class Admin::ContentController < Admin::BaseController
 
   def edit
     @article = Article.find(params[:id])
+    @admin = Profile.find(current_user.profile_id).label == "admin"
     unless @article.access_by? current_user
       redirect_to :action => 'index'
       flash[:error] = _("Error, you are not allowed to perform this action")
